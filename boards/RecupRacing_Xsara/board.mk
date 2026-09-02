bacace4f0657f916c7ff6d47c6c08a2ab1e59966
elonmike07 <hugo070601@hotmail.fr>
2026-09-02 18:43:15 +0200

    Bootloader update

--- boards/RecupRacing_Xsara/board.mk
+++ boards/RecupRacing_Xsara/board.mk
@@ -1,6 +1,9 @@
 # Activation de la pile logicielle pour le bus CAN
 EFI_CAN_SUPPORT = yes
 
+# Activation du bootloader OpenBLT (décale l'adresse de liaison à 0x08010000)
+USE_OPENBLT = yes
+
 ifneq ($(PROJECT_CPU),simulator)
 BOARDCPPSRC += \
     $(BOARD_DIR)/board_configuration.cpp \
